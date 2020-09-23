d4d134f7d8e6ca47d44c681f9862be19b8cde4ee
craigmd <craigmichaeldickson@gmail.com>
2020-09-22 22:32:47 -0400

    config, deps, etc

---
+++ src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './Card.module.css';
+
+export function Card() {
+  return <div className={styles.card} />;
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.module.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+                exportLocalsConvention: 'camelCase',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
